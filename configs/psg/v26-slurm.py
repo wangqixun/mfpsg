756eedb004f0ab481035ce2dc7680c4bcde6189f
王奇勋 <wangqixun@xiaohongshu.com>
2022-09-18 23:32:30 +0800

    test cfg

--- configs/psg/v26-slurm.py
+++ configs/psg/v26-slurm.py
@@ -220,7 +220,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1500, 1500),
+        img_scale=(1333, 800),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
